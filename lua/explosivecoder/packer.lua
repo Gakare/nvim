7657b502bdb394443a9397a239de8e326527a635
gakare <chiong_patrick@yahoo.com>
2023-12-21 00:07:58 -0800

    I finally got a baseline for my debugger. I configured my cpp debugger

--- lua/explosivecoder/packer.lua
+++ lua/explosivecoder/packer.lua
@@ -53,11 +53,21 @@ return require('packer').startup(function(use)
     use 'tpope/vim-fugitive'
 
     use {
-        'akinsho/toggleterm.nvim', tag = '*', config = function()
+        'akinsho/toggleterm.nvim', config = function()
             require("toggleterm").setup()
         end
     }
 
     use 'mbbill/undotree'
+
+    use {
+        'mfussenegger/nvim-dap',
+        requires = {
+            'theHamsta/nvim-dap-virtual-text',
+            'rcarriga/nvim-dap-ui',
+            'nvim-telescope/telescope-dap.nvim',
+            'folke/neodev.nvim'
+        }
+    }
 
 end)
