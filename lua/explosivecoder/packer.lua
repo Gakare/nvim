7de711815ee5d959f46ac8c52584cf329919f467
gakare <chiong_patrick@yahoo.com>
2024-10-12 01:23:16 -0700

    Fixed my luasnip sourcing. Should no longer matter what system I am using.

--- lua/explosivecoder/packer.lua
+++ lua/explosivecoder/packer.lua
@@ -4,89 +4,89 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use({ "wbthomason/packer.nvim" })
+    -- Packer can manage itself
+    use({ "wbthomason/packer.nvim" })
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "sharkdp/fd" },
-			{ "BurntSushi/ripgrep" },
-		},
-	})
+    use({
+        "nvim-telescope/telescope.nvim",
+        requires = {
+            { "nvim-lua/plenary.nvim" },
+            { "sharkdp/fd" },
+            { "BurntSushi/ripgrep" },
+        },
+    })
 
-	-- Color scheme
-	use({ "dasupradyumna/midnight.nvim" })
+    -- Color scheme
+    use({ "dasupradyumna/midnight.nvim" })
 
-	use({ "nvim-treesitter/nvim-treesitter" })
+    use({ "nvim-treesitter/nvim-treesitter" })
 
-	use({
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+    use({
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		requires = {
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        requires = {
 
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "L3MON4D3/LuaSnip" },
-			-- luasnip completion source for nvim-cmp
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-path" },
-		},
-	})
+            -- LSP Support
+            { "neovim/nvim-lspconfig" },
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "L3MON4D3/LuaSnip" },
+            -- luasnip completion source for nvim-cmp
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-path" },
+        },
+    })
 
-	use({ "tpope/vim-fugitive" })
+    use({ "tpope/vim-fugitive" })
 
-	use({
-		"akinsho/toggleterm.nvim",
-		config = function()
-			require("toggleterm").setup()
-		end,
-	})
+    use({
+        "akinsho/toggleterm.nvim",
+        config = function()
+            require("toggleterm").setup()
+        end,
+    })
 
-	use({ "mbbill/undotree" })
+    use({ "mbbill/undotree" })
 
-	use({
-		"mfussenegger/nvim-dap",
-		requires = {
-			"theHamsta/nvim-dap-virtual-text",
-			"rcarriga/nvim-dap-ui",
-			"nvim-telescope/telescope-dap.nvim",
-			"folke/neodev.nvim",
-			"nvim-telescope/telescope-dap.nvim", -- I am trying to see if I need this
-			"folke/neodev.nvim",
-			"mortepau/codicons.nvim",
-			"nvim-telescope/telescope-dap.nvim",
-			"folke/neodev.nvim",
-			"nvim-neotest/nvim-nio",
-		},
-	})
+    use({
+        "mfussenegger/nvim-dap",
+        requires = {
+            "theHamsta/nvim-dap-virtual-text",
+            "rcarriga/nvim-dap-ui",
+            "nvim-telescope/telescope-dap.nvim",
+            "folke/neodev.nvim",
+            "nvim-telescope/telescope-dap.nvim", -- I am trying to see if I need this
+            "folke/neodev.nvim",
+            "mortepau/codicons.nvim",
+            "nvim-telescope/telescope-dap.nvim",
+            "folke/neodev.nvim",
+            "nvim-neotest/nvim-nio",
+        },
+    })
 
-	use({ "nvim-tree/nvim-web-devicons" })
+    use({ "nvim-tree/nvim-web-devicons" })
 
-	use({ "folke/trouble.nvim" })
+    use({ "folke/trouble.nvim" })
 
-	use({ "j-hui/fidget.nvim" })
+    use({ "j-hui/fidget.nvim" })
 
-	use({ "stevearc/conform.nvim" })
+    use({ "stevearc/conform.nvim" })
 
-	use({ "mfussenegger/nvim-lint" })
+    use({ "mfussenegger/nvim-lint" })
 
-	use({ "prichrd/netrw.nvim" })
+    use({ "prichrd/netrw.nvim" })
 
-	use({ "nvim-lualine/lualine.nvim" })
+    use({ "nvim-lualine/lualine.nvim" })
 
-	use({ "norcalli/nvim-colorizer.lua" })
+    use({ "norcalli/nvim-colorizer.lua" })
 end)
