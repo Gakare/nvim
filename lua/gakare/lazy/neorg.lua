1e94a49dd8c5e732994a497e64eeed2ecf094a1d
gakare <gakare@baddie.com>
2025-12-22 00:17:17 -0800

    Updated treesitter to use gcc as the compiler instead of zig.

--- lua/gakare/lazy/neorg.lua
+++ lua/gakare/lazy/neorg.lua
@@ -1,15 +1,13 @@
 return {
-	{
-		"nvim-neorg/neorg",
-		lazy = false,
-		version = "*",
-		config = function()
-			require("neorg").setup({
-				load = {
-					["core.defaults"] = {},
-					["core.concealer"] = {},
-				},
-			})
-		end,
-	},
+    {
+        "nvim-neorg/neorg",
+        lazy = false,
+        version = "*",
+        config = {
+            load = {
+                ["core.defaults"] = {},
+                ["core.concealer"] = {},
+            }
+        },
+    },
 }
