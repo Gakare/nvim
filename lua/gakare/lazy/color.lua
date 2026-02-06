8387906d15823b76dc4fd81fa44b98139bdf1163
gakare <gakare@baddie.com>
2026-02-06 13:26:18 -0800

    Fixed color.lua

--- lua/gakare/lazy/color.lua
+++ lua/gakare/lazy/color.lua
@@ -1,11 +1,6 @@
 local function colorscheme(color)
-<<<<<<< HEAD
-	color = color or "default"
-	vim.cmd.colorscheme(color)
-=======
-    color = color or "midnight"
+    color = color or "default"
     vim.cmd.colorscheme(color)
->>>>>>> 189db6bc06c014a59d26dab4a7d2989ffe420751
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
@@ -13,24 +8,22 @@ local function colorscheme(color)
 end
 
 return {
-<<<<<<< HEAD
-	{
-		"EdenEast/nightfox.nvim",
-		lazy = false,
-		opts = {},
-		priority = 1000,
-		config = function()
-			colorscheme("carbonfox")
-		end,
-	},
-	{
-		"norcalli/nvim-colorizer.lua",
-		lazy = false,
-		config = function()
-			require("colorizer").setup({ "*" })
-		end,
-	},
-=======
+    {
+        "EdenEast/nightfox.nvim",
+        lazy = false,
+        opts = {},
+        priority = 1000,
+        config = function()
+            colorscheme("carbonfox")
+        end,
+    },
+    {
+        "norcalli/nvim-colorizer.lua",
+        lazy = false,
+        config = function()
+            require("colorizer").setup({ "*" })
+        end,
+    },
     {
         "dasupradyumna/midnight.nvim",
         lazy = false,
@@ -47,5 +40,4 @@ return {
             require("nvim-highlight-colors").setup({})
         end,
     },
->>>>>>> 189db6bc06c014a59d26dab4a7d2989ffe420751
 }
