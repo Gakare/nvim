f9e26ea63659e02cb3020c90abfc810e95a32218
Gakare <chiong_patrick@yahoo.com>
2026-02-06 17:46:08 -0800

    Removed nvim-colorizer

--- lua/gakare/lazy/color.lua
+++ lua/gakare/lazy/color.lua
@@ -1,19 +1,13 @@
 local function colorscheme(color)
-<<<<<<< HEAD
 	color = color or "default"
 	vim.cmd.colorscheme(color)
-=======
-    color = color or "midnight"
-    vim.cmd.colorscheme(color)
->>>>>>> 189db6bc06c014a59d26dab4a7d2989ffe420751
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "Comment", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Comment", { bg = "none" })
 end
 
 return {
-<<<<<<< HEAD
 	{
 		"EdenEast/nightfox.nvim",
 		lazy = false,
@@ -24,28 +18,19 @@ return {
 		end,
 	},
 	{
-		"norcalli/nvim-colorizer.lua",
+		"dasupradyumna/midnight.nvim",
 		lazy = false,
+		opts = {},
+		priority = 1000,
 		config = function()
-			require("colorizer").setup({ "*" })
+			colorscheme()
 		end,
 	},
-=======
-    {
-        "dasupradyumna/midnight.nvim",
-        lazy = false,
-        opts = {},
-        priority = 1000,
-        config = function()
-            colorscheme()
-        end,
-    },
-    {
-        "brenoprata10/nvim-highlight-colors",
-        lazy = false,
-        config = function()
-            require("nvim-highlight-colors").setup({})
-        end,
-    },
->>>>>>> 189db6bc06c014a59d26dab4a7d2989ffe420751
+	{
+		"brenoprata10/nvim-highlight-colors",
+		lazy = false,
+		config = function()
+			require("nvim-highlight-colors").setup({})
+		end,
+	},
 }
